33d7b4a6592450b4042094567f661b156002a281
Orlando <oevasque@gmail.com>
2013-11-01 19:39:44 -0300

    hola

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hola::Application.config.secret_token = '06e866f0f67e00d2d872af1e1d21cdd99cf9d8f96cf0c73a7a8b730524eb509c27ecdad1ac2c479fe31cdd6681b3097436ef6dba447de7a55bf46c98f0c85fe0'
